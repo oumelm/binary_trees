e0e51b7a59cd25f06e4282cfaac4888c836b58aa
oumelm <omaymaelmrhasli@gmail.com>
2023-11-03 10:32:01 -0700

    B

---
+++ tests/10-main.c
@@ -0,0 +1,31 @@
+Impossible de créer, modifier ou importer … Vous n'avez pas assez d'espace de stockage pour créer, modifier ou importer des fichiers. Profitez de 100 Go de stockage pour 19,00 MAD 4,75 MAD/mois pendant 3 mois.
+1-main.c
+#include <stdlib.h>
+#include <stdio.h>
+#include "../binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    binary_tree_t *root;
+    size_t depth;
+
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 402);
+    binary_tree_insert_right(root->left, 54);
+    binary_tree_insert_right(root, 128);
+    binary_tree_print(root);
+
+    depth = binary_tree_depth(root);
+    printf("Depth of %d: %lu\n", root->n, depth);
+    depth = binary_tree_depth(root->right);
+    printf("Depth of %d: %lu\n", root->right->n, depth);
+    depth = binary_tree_depth(root->left->right);
+    printf("Depth of %d: %lu\n", root->left->right->n, depth);
+    return (0);
+}
